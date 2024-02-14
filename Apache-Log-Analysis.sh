7a773bc5c1447493a441ee92cb3e2303c64585d7
ownux-infosec <76550520+ownux-infosec@users.noreply.github.com>
2024-02-14 17:57:53 +0530

    Update Apache-Log-Analysis.sh
    Updated

--- Apache-Log-Analysis.sh
+++ Apache-Log-Analysis.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-#								#
-#								#
+#				#
+#				#
 # Developed by Bhashit Pandya	#
-# Ownux Infosec Pvt. Ltd.		#
+# Ownux Infosec Pvt. Ltd.	#
 #################################
-
 
 echo "[+]File Name: $1"
 keywords=("root.php" "r00t" "shell" "sh3ll" "wso" "c99" "B374K" "stun.php" "w00t" "minishell" "xleet-shell")
